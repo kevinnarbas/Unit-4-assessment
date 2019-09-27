cf2fc14beb30a927952343e0831c3b5c28b6db64
kevinnarbas <kevinnarbas@git.generalassemb.ly>
2019-09-27 13:58:36 -0500

    Updates

---
+++ precache-manifest.6d55ec5a0c0f0ac675dd318d13b11ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa4e02c5d7a956ce433605251000e20",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/index.html"
+  },
+  {
+    "revision": "6f8e56f268b97bcff79d",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/css/main.7f68111c.chunk.css"
+  },
+  {
+    "revision": "3ba513cd2dbec98c3b41",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/2.7b44ccab.chunk.js"
+  },
+  {
+    "revision": "6f8e56f268b97bcff79d",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/main.d0905823.chunk.js"
+  },
+  {
+    "revision": "8cc6533f46336e08e7ae",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/runtime-main.6be37d5a.js"
+  }
+]);
